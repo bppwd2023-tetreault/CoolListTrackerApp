5e28ec345eb5a0e0cb9b1ca258ef5d3a2ec48c13
Alexzander Tetreault <ar.tetreault@baypath.net>
2022-12-01 14:18:38 +0000

    Added a name parameter to the user

--- db/schema.rb
+++ db/schema.rb
@@ -29,17 +29,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_174736) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "students", force: :cascade do |t|
-    t.string "name"
-    t.integer "grade"
-    t.decimal "gpa"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "John Doe", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
